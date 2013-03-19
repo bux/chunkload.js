a11429e872dc66564083054cff87de16564967dc
Jo David <github@jonathandavid.de>
2013-03-19 09:18:37 +0100

    init

---
+++ chunkload.js
@@ -0,0 +1,100 @@
+var Downloader;
+(function (Downloader) {
+    var ChunkLoader = (function () {
+        function ChunkLoader(fileGuid, requestUrl, chunkCountMethod, getChunkMethod, fileGuidParamName, chunkNrParamName) {
+            this.m_fileGuid = fileGuid;
+            this.m_requestUrl = requestUrl;
+            this.m_chunkCountMethod = chunkCountMethod;
+            this.m_getChunkMethod = getChunkMethod;
+            this.m_fileGuidParamName = fileGuidParamName;
+            this.m_chunkNrParamName = chunkNrParamName;
+        }
+        ChunkLoader.prototype.startDownload = function (callback) {
+            var that = this;
+            this.m_allChunkDownloadedCallback = callback;
+            this.m_arrBlobs = [];
+            this.m_chunkQueue = [];
+            this.getChunkCount(that.onChunkCountRecieved);
+        };
+        ChunkLoader.prototype.getChunkCount = function (callback) {
+            var that = this;
+            var params = this.m_fileGuidParamName + "=" + this.m_fileGuid;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", this.m_requestUrl + this.m_chunkCountMethod + "?" + params, true);
+            xhr.onload = function () {
+                callback(this, that);
+            };
+            xhr.send(null);
+        };
+        ChunkLoader.prototype.downloadChunks = function (chunkCount, fileGuid) {
+            for(var i = 1; i <= chunkCount; i++) {
+                var chunk = new Chunk(i, fileGuid);
+                this.m_chunkQueue.push(chunk);
+            }
+            this.downloadNextChunk();
+        };
+        ChunkLoader.prototype.downloadNextChunk = function () {
+            if(this.m_chunkQueue.length > 0) {
+                var nextChunk = this.m_chunkQueue.shift();
+                this.downloadChunk(nextChunk);
+            } else {
+                this.m_chunkQueue = null;
+            }
+        };
+        ChunkLoader.prototype.downloadChunk = function (chunk) {
+            var that = this;
+            var params = this.m_fileGuidParamName + "=" + chunk.fileGuid + "&" + this.m_chunkNrParamName + "=" + chunk.chunkNr;
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", this.m_requestUrl + this.m_getChunkMethod, true);
+            xhr.responseType = "blob";
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.onload = function () {
+                that.onChunkDownloaded(this, chunk);
+            };
+            xhr.send(params);
+        };
+        ChunkLoader.prototype.onChunkCountRecieved = function (xhr, that) {
+            that.m_chunkCount = parseInt(xhr.responseText);
+            that.downloadChunks(that.m_chunkCount, that.m_fileGuid);
+        };
+        ChunkLoader.prototype.onChunkDownloaded = function (xhr, downloadedChunk) {
+            this.m_arrBlobs.push(xhr.response);
+            if(this.m_chunkQueue.length === 0) {
+                this.onAllChunksDownloaded();
+            } else {
+                this.downloadNextChunk();
+            }
+            xhr = null;
+        };
+        ChunkLoader.prototype.onAllChunksDownloaded = function () {
+            var finalBlob = new Blob(this.m_arrBlobs, {
+                type: this.m_arrBlobs[0].type
+            });
+            this.m_arrBlobs = null;
+            this.m_allChunkDownloadedCallback(finalBlob);
+        };
+        return ChunkLoader;
+    })();    
+    var Chunk = (function () {
+        function Chunk(chunkNr, fileGuid) {
+            this.m_chunkNr = chunkNr;
+            this.m_fileGuid = fileGuid;
+        }
+        Object.defineProperty(Chunk.prototype, "chunkNr", {
+            get: function () {
+                return this.m_chunkNr;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Chunk.prototype, "fileGuid", {
+            get: function () {
+                return this.m_fileGuid;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Chunk;
+    })();
+    Downloader.Chunk = Chunk;    
+})(Downloader || (Downloader = {}));
---
+++ chunkload.ts
@@ -0,0 +1,189 @@
+module Downloader {
+
+     class ChunkLoader { 
+
+        private m_arrBlobs: any[];
+        private m_chunkQueue: Chunk[];
+        private m_fsh: FileSystemHandler;
+
+        private m_fileGuid: string;
+        private m_requestUrl: string;
+        private m_chunkCountMethod: string;
+        private m_getChunkMethod: string;
+        private m_fileGuidParamName: string;
+        private m_chunkNrParamName: string;
+
+        private m_chunkCount: number;
+
+        private m_allChunkDownloadedCallback: (fileBlob:any) => void;
+
+
+        /*
+        *       ########## CTOR ##########
+        */
+
+        // ctor
+        constructor(fileGuid: string, requestUrl: string, chunkCountMethod: string, getChunkMethod: string, fileGuidParamName: string, chunkNrParamName:string) {
+            this.m_fileGuid = fileGuid;
+
+            this.m_requestUrl = requestUrl;
+            this.m_chunkCountMethod = chunkCountMethod;
+            this.m_getChunkMethod = getChunkMethod;
+
+            this.m_fileGuidParamName = fileGuidParamName;
+            this.m_chunkNrParamName = chunkNrParamName;
+        }
+
+
+
+        /*
+        *       ########## PUBLIC ##########
+        */
+
+        public startDownload(callback:(fileBlob:any)=>void): void {
+
+            var that = this;
+            
+            this.m_allChunkDownloadedCallback = callback;
+            this.m_arrBlobs = [];
+            this.m_chunkQueue = [];
+
+            this.getChunkCount(that.onChunkCountRecieved);
+
+        }
+
+
+
+        /*
+        *       ########## PRIVATE ##########
+        */
+
+        private getChunkCount(callback: (xhr:XMLHttpRequest, that:ChunkLoader) => void ):void {
+
+            var that = this;
+
+            var params: string = this.m_fileGuidParamName + "=" + this.m_fileGuid;
+
+            // get the chunk count from server
+            var xhr: XMLHttpRequest = new XMLHttpRequest();
+            xhr.open("GET", this.m_requestUrl + this.m_chunkCountMethod + "?" + params, true);
+
+            // callback function for the onload event
+            xhr.onload = function () {
+
+                // here: this is the scope of the xhr callback
+                // so "this" is the xhr object and not the ChunkLoader class
+                callback(this, that);
+            };
+
+            // send the request
+            xhr.send(null);
+        }
+
+        private downloadChunks(chunkCount: number, fileGuid: string): void {
+
+            // fill the chunkQueue
+            for (var i = 1; i <= chunkCount; i++) {
+                var chunk = new Chunk(i, fileGuid);
+                this.m_chunkQueue.push(chunk);
+            }
+
+            // then start downloading
+            this.downloadNextChunk();
+
+        }
+
+        private downloadNextChunk(): void {
+
+            if (this.m_chunkQueue.length > 0) {
+                // Array.shift() returns the topmost entry and removes it from the array
+                // therefore newLength = oldLength - 1
+                var nextChunk: Chunk = this.m_chunkQueue.shift();
+                this.downloadChunk(nextChunk);
+            } else {
+                this.m_chunkQueue = null;
+            }
+
+        }
+
+        private downloadChunk(chunk: Chunk): void {
+            
+            var that = this;
+
+            var params: string = this.m_fileGuidParamName + "=" + chunk.fileGuid + "&" + this.m_chunkNrParamName + "=" + chunk.chunkNr;
+
+            var xhr : XMLHttpRequest = new XMLHttpRequest();
+
+            xhr.open("POST", this.m_requestUrl + this.m_getChunkMethod, true);
+            xhr.responseType = "blob";
+
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+            xhr.onload = function () {
+                that.onChunkDownloaded(this, chunk);
+            };
+
+            xhr.send(params);
+
+        }
+
+        /*
+        *       ########## EVENTS ##########
+        */
+
+        private onChunkCountRecieved(xhr: XMLHttpRequest, that: ChunkLoader): void {
+            that.m_chunkCount = parseInt(xhr.responseText);
+            
+            that.downloadChunks(that.m_chunkCount, that.m_fileGuid);
+        }
+
+
+        private onChunkDownloaded(xhr: XMLHttpRequest, downloadedChunk: Chunk): void {
+
+            this.m_arrBlobs.push(xhr.response);
+
+            if (this.m_chunkQueue.length === 0) {
+                this.onAllChunksDownloaded(/*downloadedChunk.fileGuid*/);
+            } else {
+                this.downloadNextChunk();
+            }
+
+            xhr = null;
+
+        }
+
+        private onAllChunksDownloaded(): void {
+
+            var finalBlob:Blob = new Blob(this.m_arrBlobs, { type: this.m_arrBlobs[0].type });
+
+            this.m_arrBlobs = null;
+
+            this.m_allChunkDownloadedCallback(finalBlob);
+
+        }
+
+
+    }
+
+    export class Chunk {
+
+        private m_chunkNr: number;
+
+        get chunkNr(): number {
+            return this.m_chunkNr;
+        }
+
+        private m_fileGuid: string;
+
+        get fileGuid(): string {
+            return this.m_fileGuid;
+        }
+
+        constructor(chunkNr: number, fileGuid: string) {
+            this.m_chunkNr = chunkNr;
+            this.m_fileGuid = fileGuid;
+        }
+
+    }
+
+}
